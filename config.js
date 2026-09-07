975fb6f8bc4ea093f9ff671d1b52e45e17bc486f
cr596510-ux <cr596510@gmail.com>
2026-09-07 22:41:56 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,25 @@
 export const CONFIG = Object.freeze({
   app: {
-    name: "ZAKSH",
+    name: "ZAKSH Mining",
     tokenSymbol: "ZKO"
-  },
-
-  economy: {
-    usdPrice: 0.13,
-    miningRewardMin: 60,
-    miningRewardMax: 160,
-    taskReward: 4,
-    adTaskReward: 3
   },
 
   mining: {
     durationSeconds: 40,
-    maxSessions: 2,
-    cooldownHours: 6
+    cooldownHours: 6,
+    maxSessions: 2
+  },
+
+  economy: {
+    miningRewardMin: 60,
+    miningRewardMax: 160
   },
 
   tasks: {
-    count: 4,
     cooldownHours: 2
   },
 
   campaign: {
-    endAt: "2027-01-05T23:59:59.000Z"
-  },
-
-  withdrawal: {
-    enabledByOwner: false
+    endAt: "2027-01-05T00:00:00.000Z"
   }
 });
